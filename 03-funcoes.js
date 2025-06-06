30fc77a79b219dc56444086099996135cafb19d3
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-06 09:20:29 -0300

    Aplica função separador

--- 03-funcoes.js
+++ 03-funcoes.js
@@ -11,3 +11,27 @@ function exemplo1(){
 
 exemplo1(); // chamada da função
 
+
+function separador(){
+    console.log("\n********************************\n");
+}
+
+separador();
+
+// Chamada aqui dá erro, pois não pode ser feita antes da declaração
+// exemplo2(); 
+
+/* Sintaxe tradicional, função anônima
+Nesta sintaxe, a ordem de declaração IMPORTA. */
+const exemplo2 = function(){
+    console.log("Função anônima");
+};
+
+exemplo2(); // chamada (sempre após declarar a função anônima)
+
+
+separador();
+
+
+
+
