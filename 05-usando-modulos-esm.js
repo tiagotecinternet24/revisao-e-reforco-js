42c735cb922a3e35984f720de06979e657310d4a
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-09 09:21:51 -0300

    Create 05-usando-modulos-esm.js

---
+++ 05-usando-modulos-esm.js
@@ -0,0 +1,8 @@
+'use strict';
+// 05-usando-modulos-esm.js
+
+// Importação de múltiplos recursos/módulos usando ESM
+import { saudacao, clientes } from './modulos-esm/exemplos.js';
+
+saudacao("Fulano de Tal");
+console.log(clientes);
