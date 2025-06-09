0bbc844d99a89db9271bb2d52c3e18a742f03e6a
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-09 09:52:06 -0300

    Cria e exporta novos módulos

---
+++ modulos-esm/dados.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const pessoa = {
+    nome: 'Ian Gillan',
+    idade: 78,
+    cidade: 'São Paulo'
+};
+
+const livro = {
+    titulo: 'O Senhor dos Anéis',
+    volume: 'As Duas Torres',
+    ano: 1954
+};
+
+const alunos = ['Pica Pau', 'Zé Colméia', 'Naruto', 'Luffy'];
+
+export { pessoa, livro, alunos }
