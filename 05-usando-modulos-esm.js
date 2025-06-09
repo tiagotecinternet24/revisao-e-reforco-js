3608a3048fbdc89b515d16a09ddf25ee548bdcb2
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-09 10:11:25 -0300

    Acessa recursos do módulo com alias

--- 05-usando-modulos-esm.js
+++ 05-usando-modulos-esm.js
@@ -7,6 +7,10 @@ import alunos from './modulos-esm/alunos.js';
 // Importação de múltiplos recursos/módulos usando ESM
 import { saudacao, clientes, separador } from './modulos-esm/exemplos.js';
 
+// Uso de um 'alias/apelido' para evitar conflito de nomes
+import { pessoa, livro, alunos as personagens } from './modulos-esm/dados.js';
+
+
 saudacao("Fulano de Tal");
 console.log(clientes);
 
@@ -19,5 +23,13 @@ console.log(alunos);
 for(const aluno of alunos){
     console.log(aluno);    
 }
+
+
+separador();
+
+// Acessando recursos do módulo 'dados.js'
+console.log(pessoa.nome);
+console.log(livro);
+console.log(personagens); // lembre-se: personagens é um apelido para alunos
 
 
