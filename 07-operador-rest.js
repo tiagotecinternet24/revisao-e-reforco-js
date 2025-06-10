fe9216a29ba9e8f9425ee66b36e61bb423925b15
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-10 11:13:59 -0300

    Update 07-operador-rest.js

--- 07-operador-rest.js
+++ 07-operador-rest.js
@@ -1,9 +1,10 @@
 'use strict';
 
+import { classificar } from './modulos-esm/funcoes.js';
+
 /* Sobre o operador rest  ...
 Usamos o rest para mesclar uma relação de parâmetros/argumentos 
-para uma função. Na prática, os parâmetros se comportam como um array podendo receber
-iterações via loop. */
+para uma função. Na prática, os parâmetros se comportam como um array podendo receber iterações via loop. */
 function somar(...valores){
     // Variável acumuladora
     let total = 0;
@@ -20,3 +21,14 @@ let resultado1 = somar(10, 20);
 let resultado2 = somar(10, 20, 50);
 console.log(resultado1);
 console.log(resultado2);
+
+
+console.log("----------------");
+
+console.log( classificar("Goiaba", "Amora", "Banana", "Abacate") );
+console.log( 
+    classificar(
+        "Yes", "Rush", "Pink Floyd", "Slayer", "Iron Maiden", "Metallica"
+    ) 
+);
+
