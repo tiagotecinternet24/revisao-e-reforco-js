1af14c9f74faf691a0abb11650700c52163d1bfd
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-11 08:47:08 -0300

    Desestrutura parcialmente

--- 09-desestruturacao.js
+++ 09-desestruturacao.js
@@ -10,3 +10,19 @@ const [cliente1, cliente2, cliente3] = clientes;
 console.log(cliente1);
 console.log(cliente2);
 console.log(cliente3);
+
+separador();
+
+const capitais = [
+  "João Pessoa",
+  "Manaus",
+  "Curitiba",
+  "Salvador",
+  "Belo Horizonte",
+];
+
+// Desestruturando alguns valores (omitimos Curitiba e Belo Horizonte)
+const [paraiba, amazonas, , bahia] = capitais;
+console.log(paraiba);
+console.log(amazonas);
+console.log(bahia);
