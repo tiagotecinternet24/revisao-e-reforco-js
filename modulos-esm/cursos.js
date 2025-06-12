a817f16a19c0f53cca5ac2ce07b9bf09cacfc79e
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-12 10:31:37 -0300

    Create cursos.js

---
+++ modulos-esm/cursos.js
@@ -0,0 +1,19 @@
+const cursos = [
+    { id: 1, titulo: "HTML5 e CSS3", categoria: "Front-End", preco: 500 },
+    { id: 2, titulo: "JS e React", categoria: "Front-End", preco: 800 },
+    { id: 3, titulo: "React Native", categoria: "Mobile", preco: 1000 },
+    { id: 4, titulo: "Photoshop", categoria: "Design", preco: 400 },
+    { id: 5, titulo: "PHP e MySQL", categoria: "Back-End", preco: 600 },
+    { id: 6, titulo: "Flutter", categoria: "Mobile", preco: 900 },
+    { id: 7, titulo: "Figma", categoria: "Design", preco: 100 },
+    { id: 8, titulo: "TypeScript moderno", categoria: "Front-End", preco: 700 },
+    { id: 9, titulo: "Node.js e Express", categoria: "Back-End", preco: 850 },
+    { id: 10, titulo: "UI/UX Design", categoria: "Design", preco: 500 },
+    { id: 11, titulo: "Kotlin para Android", categoria: "Mobile", preco: 950 },
+    { id: 12, titulo: "Laravel com MySQL", categoria: "Back-End", preco: 750 },
+    { id: 13, titulo: "Vue.js do Zero", categoria: "Front-End", preco: 650 },
+    { id: 14, titulo: "Ilustração Digital", categoria: "Design", preco: 450 },
+    { id: 15, titulo: "Swift para iOS", categoria: "Mobile", preco: 980 }
+];
+
+export default cursos;
