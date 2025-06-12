d817618e1b69c13718d1afef9822d0b2b29cb458
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-12 10:31:34 -0300

    Aplica map básico em arrays simples

---
+++ 10-map.js
@@ -0,0 +1,29 @@
+"use strict";
+
+import { separador } from "./modulos-esm/exemplos.js";
+
+/* map (mapear, mapeamento - transformar/gerar em outra coisa) 
+Passa por elementos de um array e realiza operações em cada um deles
+através de uma função callback, gerando um novo array de elementos 
+transformados. */
+
+// Exemplo 1
+const numeros = [10, 20, 5, 12.5, 66, 50, 1000];
+console.log(numeros);
+
+const numerosDobrados = numeros.map((numero) => numero * 2);
+console.log(numerosDobrados);
+
+separador();
+
+/* Mini-exercício:
+1) Crie um array com 5 textos escritos em letras MAIÚSCULAS
+2) Mostre no console este array
+3) Usando o map, acesse e transforme todos eles em letras minúsculas (toLowerCase)
+4) Guarde o resultado do map em uma nova constante 
+5) Mostre no console este novo array */
+
+const textos = ["A", "B", "C", "D", "E"];
+console.log(textos);
+const novoTextos = textos.map((texto) => texto.toLowerCase());
+console.log(novoTextos);
